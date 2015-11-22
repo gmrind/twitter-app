97c7b9d6734cd091f518f55979208910cfdb5e0c
gmrind <murtazarind.11@gmail.com>
2015-11-22 23:08:35 +0500

    Gamil Eamil Setup in Production Evoirment

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,10 +1,10 @@
-ActionMailer::Base.delivery_method = :smtp
-ActionMailer::Base.smtp_settings = {
-    :address 								=> "smtp.sendgrid.net",
-    :port 									=> "587",
-    :domain 								=> "heroku.com",
-    :user_name							=> "app44191126@heroku.com",
-    :password 							=> "jgwp6pjj0388",
-    :authentication 				=> :plain,
-    :enable_starttls_auto 	=> true
-}
+# ActionMailer::Base.delivery_method = :smtp
+# ActionMailer::Base.smtp_settings = {
+#     :address 								=> "smtp.sendgrid.net",
+#     :port 									=> "587",
+#     :domain 								=> "heroku.com",
+#     :user_name							=> "app44191126@heroku.com",
+#     :password 							=> "jgwp6pjj0388",
+#     :authentication 				=> :plain,
+#     :enable_starttls_auto 	=> true
+# }
--- config/initializers/smtp_settings.rb
+++ config/initializers/smtp_settings.rb
@@ -1,9 +1,10 @@
-# ActionMailer::Base.smtp_settings = {
-#     :address => "smtp.gmail.com",
-#     :port => "587",
-#     :domain => "mydomain.net",
-#     :user_name => "gm.tweettoo@gmail.com",
-#     :password => "gmrind.786",
-#     :authentication => "plain",
-#     :enable_starttls_auto => true
-# }
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    :address => "smtp.gmail.com",
+    :port => "587",
+    :domain => "gmail.com",
+    :user_name => "gm.tweettoo@gmail.com",
+    :password => "gmrind.786",
+    :authentication => "plain",
+    :enable_starttls_auto => true
+}
